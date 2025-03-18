51295e29723e60d67862e54d6085df33df35090f
BrahiamAcosta <brahiam.acosta@udea.edu.co>
2025-03-17 19:52:35 -0500

    highlight memory cells on value change

--- frontend/src/components/MemoryTable.jsx
+++ frontend/src/components/MemoryTable.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react";
 import "../styles/MemoryTable.css";
 
 function MemoryTable({ memory }) {
+  const [previousMemory, setPreviousMemory] = useState([]);
+
+  const [highlightedCells, setHighlightedCells] = useState({});
+
+  useEffect(() => {
+    const newHighlights = {};
+
+    memory.forEach(([key, value], index) => {
+      const prevValue = previousMemory[index]?.[1];
+
+      if (value !== prevValue) {
+        newHighlights[key] = true;
+
+        setTimeout(() => {
+          setHighlightedCells((prev) => ({ ...prev, [key]: false }));
+        }, 800);
+      }
+    });
+
+    setHighlightedCells((prev) => ({ ...prev, ...newHighlights }));
+    setPreviousMemory(memory);
+  }, [memory]);
+
   return (
     <div className="memory_table">
       <table>
@@ -14,7 +38,9 @@ function MemoryTable({ memory }) {
           {memory.map(([key, value], index) => (
             <tr key={index}>
               <td>{key}</td>
-              <td>{value}</td>
+              <td className={highlightedCells[key] ? "highlight" : ""}>
+                {value}
+              </td>
             </tr>
           ))}
         </tbody>
@@ -22,4 +48,5 @@ function MemoryTable({ memory }) {
     </div>
   );
 }
+
 export default MemoryTable;
